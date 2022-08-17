41657e367191462f36ba80dd332fe9566b12b8d6
Yari-Carelli <yaricarelli@hotmail.it>
2022-08-17 16:12:41 +0000

    Define play function for the actual interactive gameplay

--- run.py
+++ run.py
@@ -4,3 +4,10 @@ from words import words
 def get_word():
     word = random.choice(word_list)
     return word.upper()
+
+    def play(word):
+    word_completion = "_" * len(word)
+    guessed = False
+    guessed_letters = []
+    guessed_words = []
+    tries = 6
